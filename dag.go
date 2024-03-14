a00237ae910911fbe8b4be31b825f503ba06a6c7
chenli-04 <1668648325@qq.com>
2024-03-14 23:26:43 +0800

    The first

--- dag.go
+++ dag.go
@@ -2,7 +2,33 @@ package merkledag
 
 import "hash"
 
-func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+func Add(store KVStore, node Node, h hash.Hash) ([]byte, error) {
+
+	h.Reset()
+
+	if node.Type() == FILE {
+		fileNode := node.(File)
+		data := fileNode.Bytes()
+		_, err := h.Write(data)
+		if err != nil {
+			return nil, err
+		}
+		return h.Sum(nil), nil
+	}
+
+	if node.Type() == DIR {
+		dirNode := node.(Dir)
+		it := dirNode.It()
+		for it.Next() {
+			file := it.Node().(File)
+			data := file.Bytes()
+			_, err := h.Write(data)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return h.Sum(nil), nil
+	}
+
+	return nil, nil
 }
